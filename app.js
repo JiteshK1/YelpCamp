90de4ef359eeab6ba2cf25ca02e53c0a818b3ad6
Jitesh <kanyaljitesh)@gmail.com>
2022-03-10 18:28:43 +0530

    ready to try deploying

--- app.js
+++ app.js
@@ -178,8 +178,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err: err })
 
 })
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`LISITING ON PORT ${port}`)
+    console.log(`Serving on port ${port}`)
 })
-
